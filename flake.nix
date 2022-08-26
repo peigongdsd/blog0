4c0a3b8b262bc4b819d65ae76c45fd50c9ffeefb
peigongdsd <peigong2013@outlook.com>
2022-08-26 20:29:04 +0800

    modified:   .github/workflows/azure-static-web-apps-agreeable-desert-0a1192710.yml 	modified:   .gitignore 	modified:   flake.nix 	modified:   posts/2022-08-20-Haar Measure.tex 	modified:   site.hs

--- flake.nix
+++ flake.nix
@@ -1,12 +1,23 @@
 {
   description = "Haskell Development Environment";
 
-  outputs = { self, nixpkgs }: {
-    devShell.x86_64-linux = let 
-      pkgs = nixpkgs.legacyPackages.x86_64-linux; 
-      ghc = pkgs.haskellPackages.ghcWithPackages (x : [ x.hakyll x.pandoc ]);
-      in pkgs.mkShell {
-        buildInputs = [ ghc pkgs.haskell-language-server pkgs.zlib.dev ];
-      };
-  };
+  outputs = { self, nixpkgs }: 
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+    {
+      devShell = forAllSystems (system: let 
+        pkgs = import nixpkgs { inherit system; }; 
+        ghc = pkgs.haskellPackages.ghcWithPackages (x : [ x.hakyll x.pandoc ]);
+        in pkgs.mkShell {
+          buildInputs = [ ghc pkgs.haskell-language-server pkgs.zlib.dev ];
+      });
+      defaultPackage = forAllSystems (system: let
+        pkgs = import nixpkgs { inherit system; };
+        ghc = pkgs.haskellPackages.ghcWithPackages (x : [ x.hakyll x.pandoc ]);
+        in derivation { name = "site"; builder = "${pkgs.bash}/bin/bash"; args = [ "-c" ''  
+          ${ghc}/bin/ghc -O1 -threaded -o $out ${./site.hs}
+        '' ]; inherit system; });
+    };
 }
